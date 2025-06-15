d00fb6517fb0f115d8dc10b39f128a22a663ce2b
Si-xiyu <junxiangluo0@gmail.com>
2025-06-15 12:53:55 +0800

    加入了游戏的图标

--- GuanDan/main.cpp
+++ GuanDan/main.cpp
@@ -3,12 +3,16 @@
 #include "SoundManager.h"
 #include <QApplication>
 #include <QtCore>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     qputenv("QT_MULTIMEDIA_PREFERRED_PLUGINS", "windowsmediafoundation");
 
     QApplication a(argc, argv);
+
+    // 设置图标
+    a.setWindowIcon(QIcon(":/icon/res/App_icon.png"));
     
     // 加载音量设置
     int volume = SettingsManager::loadVolume();
